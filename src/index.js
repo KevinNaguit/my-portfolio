11a358f8ec79bead8fd31012770fd80603ecf6ae
KevinNaguit <kevin.naguit@gmail.com>
2024-08-06 12:46:26 -0400

    React

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from "react-dom/client";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <main>
+    <h1>Introduction</h1>
+    <main>Hello! Welcome to My Portfolio.</main>
+    </main>
+);
